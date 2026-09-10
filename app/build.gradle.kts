e9d3209effd7f465e15e8bfe754c649b929a1ab8
wasabtc-cloud <wasa.btc@gmail.com>
2026-09-10 22:03:47 +0900

    Bump app version to 2.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.daon.futures"
         minSdk = 26
         targetSdk = 35
-        versionCode = 22
-        versionName = "2.2"
+        versionCode = 23
+        versionName = "2.3"
     }
 
     compileOptions {
