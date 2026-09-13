152adbac16fe9bad50f9552a40cc7b2c7406c37c
wasabtc-cloud <wasa.btc@gmail.com>
2026-09-13 21:56:39 +0900

    Route 1Y backtest runner to Upbit MoneyFlow V4

--- backtest/backtest_1y.py
+++ backtest/backtest_1y.py
@@ -1,4 +1,4 @@
 import os, runpy, subprocess, sys
 
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'requests'])
-runpy.run_path('backtest/upbit_moneyflow_1y.py', run_name='__main__')
+runpy.run_path('backtest/upbit_moneyflow_v4.py', run_name='__main__')
