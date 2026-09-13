c30839fc6fcf6cd80fb644aa940ab5ff989d4b0f
wasabtc-cloud <wasa.btc@gmail.com>
2026-09-13 23:14:53 +0900

    Route 1Y workflow to Upbit 100X search

--- backtest/backtest_1y.py
+++ backtest/backtest_1y.py
@@ -1,4 +1,4 @@
-import os, runpy, subprocess, sys
+import runpy, subprocess, sys
 
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'requests'])
-runpy.run_path('backtest/upbit_moneyflow_v4.py', run_name='__main__')
+runpy.run_path('backtest/youtube_10_strategies.py', run_name='__main__')
