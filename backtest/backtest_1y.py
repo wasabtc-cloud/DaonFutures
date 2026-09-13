479083e118f9719b3f1ad571c1efcd848637b64d
wasabtc-cloud <wasa.btc@gmail.com>
2026-09-13 17:07:34 +0900

    Run Upbit money-flow 1Y backtest

--- backtest/backtest_1y.py
+++ backtest/backtest_1y.py
@@ -1,6 +1,4 @@
 import os, runpy, subprocess, sys
 
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'requests'])
-runpy.run_path('backtest/youtube_10_strategies.py', run_name='__main__')
-if os.path.exists('youtube_10_results.csv'):
-    os.replace('youtube_10_results.csv', 'backtest_results.csv')
+runpy.run_path('backtest/upbit_moneyflow_1y.py', run_name='__main__')
